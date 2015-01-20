fc544740974c9bfe273efc767100d27db5b1caf0
Daniel PHAM <nguyen.pc@live.com>
2015-01-20 16:51:43 +0700

    template and migration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require foundation/foundation
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :id, :title, :description, :content, :published, :created_at, :updated_at
